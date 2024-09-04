698faec3e466a6bc5910a771b9a20def74bffc7a
Ianashek Denis <yanashek@gmail.com>
2024-09-04 17:49:39 +0700

    notifications

--- work_exchange/settings.py
+++ work_exchange/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['*',]
 
 INSTALLED_APPS = [
     'core',
-    #'notifications',
+    'notifications',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
